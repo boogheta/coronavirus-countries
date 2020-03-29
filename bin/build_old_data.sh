3de45c3eaa7b307fe5e41fb97341058f2ebd4316
RouxRC <b.ooghe@gmail.com>
2020-03-30 01:50:36 +0200

    update oldrecovered data

--- bin/build_old_data.sh
+++ bin/build_old_data.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/..
 mkdir -p data
 
 for typ in confirmed recovered deaths; do
-  curl -sL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-${typ^}.csv > data/time_series_covid19_${typ}_global.csv
+  curl -sL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_19-covid-${typ^}_archived_0325.csv > data/time_series_covid19_${typ}_global.csv
 done;
 
 ./bin/export-main-countries.py 1
