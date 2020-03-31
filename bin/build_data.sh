5fe9e892390bae52e12a54b9cde82cc9733e2a83
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-03-31 20:41:27 +0200

    add generic function for locality + include Italy data (wip #10)

--- bin/build_data.sh
+++ bin/build_data.sh
@@ -3,21 +3,30 @@
 cd $(dirname $0)/..
 mkdir -p data
 
+# World JHU data
 for typ in confirmed deaths; do
   curl -sL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_${typ}_global.csv > data/time_series_covid19_${typ}_global.csv
 done;
 
+# US JHU data
 for typ in confirmed deaths testing; do
   curl -sL https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_${typ}_US.csv > data/time_series_covid19_${typ}_US.csv
 done;
 
+# Italy official data
+curl -sL https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv > data/dpc-covid19-ita-regioni.csv
+
+# Population data
 curl -sL "https://docs.google.com/spreadsheets/d/1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0/export?format=csv&id=1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0&gid=0" > data/population-World.csv
 curl -sL "https://docs.google.com/spreadsheets/d/1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0/export?format=csv&id=1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0&gid=1662739553" > data/population-Australia.csv
 curl -sL "https://docs.google.com/spreadsheets/d/1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0/export?format=csv&id=1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0&gid=702355833" > data/population-China.csv
 curl -sL "https://docs.google.com/spreadsheets/d/1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0/export?format=csv&id=1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0&gid=88045307" > data/population-Canada.csv
+#curl -sL "https://docs.google.com/spreadsheets/d/1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0/export?format=csv&id=1e703pe3GmBQt0i2yAOS0F6Bhxy91U1-NTB6JMRSTzc0&gid=" > data/population-Italy.csv
 
+# Build agregated data for dashboard
 ./bin/export-main-countries.py
 
+# Version data
 if git diff data/*.csv | grep . > /dev/null; then
   echo "Data updated!"
   ts=$(date +%s)
--- bin/export-main-countries.py
+++ bin/export-main-countries.py
@@ -94,6 +94,11 @@ for typ in ["confirmed", "recovered", "deceased"]:
                 continue
             countries[typ][clean_region(row['Country/Region'])].append(row)
 
+# Uncomment to see which countries JHU has granularity for
+#for c, values in countries["confirmed"].items():
+#    if len(values) > 1:
+#        print c, len(values)
+
 # TODO Fix naive dates parsing
 conv = lambda d: '2020-0%s-%02d' % (d[0], int(d.split('/')[1]))
 rconv = lambda d: '%s/%s/20' % (d.split('-')[1].lstrip('0'), d.split('-')[2].lstrip('0'))
@@ -103,10 +108,8 @@ sum_values = lambda country, dat: sum([get_value(region, dat) for region in coun
 
 dates = [conv(x) for x in countries["recovered" if OLDRECOVERED else "confirmed"]["France"][0].keys() if x not in ['Lat', 'Long', 'Province/State', 'Country/Region']]
 dates.sort()
-
 if OLDRECOVERED:
     dates.pop()
-
 while not max([sum_values(countries["confirmed"][c], dates[-1]) for c in countries["confirmed"].keys()]):
     dates.pop()
 n_dates = len(dates)
@@ -116,72 +119,73 @@ data = {
     "scopes": {
       "World": {
         "level": "country",
+        "source": "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"
       },
       "China": {
         "level": "province",
+        "source": "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"
       },
-    #  "France": {
-    #    "level": "region",
-    #  },
-    #  "Italy": {
-    #    "level": "region",
-    #  },
       "Canada": {
         "level": "province",
+        "source": "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"
       },
-    #  "United Kingdom": {
-    #    "level": "country",
-    #  },
       "Australia": {
         "level": "state",
+        "source": "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"
       }
     },
     "last_update": "##LASTUPDATE##"
 }
 
-populations = {}
-for name in data["scopes"].keys():
-    with open(os.path.join("data", "population-%s.csv" % name)) as f:
-        populations[name] = {}
-        for place in csv.DictReader(f):
-            populations[name][place["id"]] = int(place["pop"])
-
 if OLDRECOVERED:
     data["scopes"]["USA"] = {
-        "level": "state"
+        "level": "state",
+        "source": "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"
     }
 
-#for c, values in countries["confirmed"].items():
-#    if len(values) > 1:
-#        print c, len(values)
+populations = {}
+def load_populations(scopes):
+    for name in scopes:
+        try:
+            with open(os.path.join("data", "population-%s.csv" % name)) as f:
+                populations[name] = {}
+                for place in csv.DictReader(f):
+                    populations[name][place["id"]] = int(place["pop"])
+        except IOError:
+            print >> sys.stderr, "WARNING: population data missing for scope", name
+load_populations(data["scopes"].keys())
 
-unit_vals = {
-    "population": 0,
-    # annotations: [],
-    "confirmed":      [0] * n_dates,
-    "deceased":       [0] * n_dates
-}
+
+def unit_vals(ndates, fieldnames, population=0):
+    unit = {
+        # annotations: [],
+        "population": population
+    }
+    for f in fieldnames:
+        unit[f] = [0] * ndates
+    return unit
+
+
+fields = ["confirmed", "deceased"]
 if OLDRECOVERED:
-    unit_vals["recovered"] = [0] * n_dates
-    unit_vals["currently_sick"] = [0] * n_dates
+    fields += ["recovered", "currently_sick"]
 
 for name, scope in data["scopes"].items():
-    level = scope["level"]
-    scope["values"] = {"total": deepcopy(unit_vals)}
+    scope["values"] = {"total": unit_vals(n_dates, fields)}
     if name == "World":
         geounits = countries["confirmed"].keys()
     else:
         geounits = countries["confirmed"][name]
     for idx, geounit in enumerate(geounits):
         c = geounit if name == "World" else clean_locality(geounit["Province/State"])
-        try:
-            pop = populations[name][c]
-        except KeyError:
-            print >> sys.stderr, "WARNING: missing population for %s / %s" % (name, c)
-            pop = 0
         if c not in scope["values"]:
-            scope["values"][c] = deepcopy(unit_vals)
-            scope["values"][c]["population"] = pop
+            try:
+                pop = populations[name][c]
+            except KeyError:
+                print >> sys.stderr, "WARNING: missing population for region %s / %s" % (name, c)
+                pop = 0
+            scope["values"][c] = unit_vals(n_dates, fields, pop)
+        scope["values"]["total"]["population"] += pop
         for i, d in enumerate(dates):
             vals = {}
             for cas in ["confirmed", "deceased"] + (["recovered"] if OLDRECOVERED else []):
@@ -192,7 +196,75 @@ for name, scope in data["scopes"].items():
                 sick = vals["confirmed"] - vals["recovered"] - vals["deceased"]
                 scope["values"][c]["currently_sick"][i] += sick
                 scope["values"]["total"]["currently_sick"][i] += sick
-        scope["values"]["total"]["population"] += pop
+
+
+localities = {
+    "Italy": {
+        "source": "https://github.com/pcm-dpc/COVID-19",
+        "filename": "dpc-covid19-ita-regioni.csv",
+        "level": "region",
+        "date_accessor": lambda row: row["data"].split("T")[0],
+        "fields": {
+            "confirmed": "totale_casi",
+            "recovered": "dimessi_guariti",
+            "deceased": "deceduti",
+            "currently_sick": "totale_attualmente_positivi",
+            "tested": "tamponi",
+            "hospitalized": "totale_ospedalizzati",
+            "intensive_care": "terapia_intensiva"
+        }
+    },
+    "France": {
+        "level": "region"
+    },
+    "United Kingdom": {
+        "level": "country"
+    }
+}
+if not OLDRECOVERED:
+    localities["US"] = {
+        "level": "state"
+    }
+
+load_populations(localities.keys())
+
+for scope, metas in localities.items():
+    if "filename" not in metas:
+        continue
+
+    data["scopes"][scope] = {
+        "level": metas["level"],
+        "source": metas["source"],
+        "dates": [],
+        "values": {}
+    }
+    with open(os.path.join("data", metas["filename"])) as f:
+        rows = list(csv.DictReader(f))
+
+        for row in rows:
+            data["scopes"][scope]["dates"].append(metas["date_accessor"](row))
+        data["scopes"][scope]["dates"] = list(set(data["scopes"][scope]["dates"]))
+        data["scopes"][scope]["dates"].sort()
+        dates_idx = {d: i for i, d in enumerate(data["scopes"][scope]["dates"])}
+        n_dates = len(data["scopes"][scope]["dates"])
+
+        fields = metas["fields"].keys()
+        data["scopes"][scope]["values"]["total"] = unit_vals(n_dates, fields, populations["World"][scope])
+        for row in rows:
+            idx = dates_idx[metas["date_accessor"](row)]
+            name = row["denominazione_regione"]
+            if name not in data["scopes"][scope]["values"]:
+                try:
+                    pop = populations[scope][name]
+                except KeyError:
+                    print >> sys.stderr, "WARNING: missing population for region %s / %s" % (scope, name)
+                    pop = 0
+                data["scopes"][scope]["values"][name] = unit_vals(n_dates, fields, pop)
+            for field in fields:
+                val = int(row[metas["fields"][field]])
+                data["scopes"][scope]["values"][name][field][idx] = val
+                data["scopes"][scope]["values"]["total"][field][idx] += val
+
 
 with open(os.path.join("data", "coronavirus-countries%s.json" % ("-oldrecovered" if OLDRECOVERED else "")), "w") as f:
     json.dump(data, f)
