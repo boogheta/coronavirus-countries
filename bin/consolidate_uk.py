f56b26076a0a95a336f29d99d4e0dbd988400d18
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-04-14 16:45:51 +0200

    handle incomplete last UK day

--- bin/consolidate_uk.py
+++ bin/consolidate_uk.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import csv
 
@@ -12,14 +15,17 @@ countries = {
 dates = []
 curdate = None
 MINDATE = "2020-01-30"
-date_idx = -1
 
 formatcase = lambda c : c.replace("ConfirmedCases", "confirmed").replace("Deaths", "deceased")
 
-def complete_last_row():
+def complete_last_row(dates, countries):
     n = len(countries["UK"]["confirmed"])
     if not n:
-        return
+        return dates, countries
+    for c in ["Wales", "Northern Ireland", "England", "Scotland"]:
+        countries[c]["confirmed"] = countries[c]["confirmed"][0:n]
+        countries[c]["deceased"] = countries[c]["deceased"][0:n]
+        dates = dates[0:n]
     for c in ["Wales", "Northern Ireland"]:
         if len(countries[c]["confirmed"]) < n:
             countries[c]["confirmed"].append(0)
@@ -27,19 +33,19 @@ def complete_last_row():
     if len(countries["England"]["confirmed"]) < n:
         for c in ["confirmed", "deceased"]:
             countries["England"][c].append(countries["UK"][c][-1] - countries["Wales"][c][-1] - countries["Scotland"][c][-1] - countries["Northern Ireland"][c][-1])
-
+    return dates, countries
 
 with open(os.path.join("data", "covid-19-indicators-uk.csv")) as f:
     for row in csv.DictReader(f):
         if row["Date"] < MINDATE or row["Indicator"] == "Tests":
             continue
         if curdate != row["Date"]:
-            complete_last_row()
+            dates, countries = complete_last_row(dates, countries)
             curdate = row["Date"]
             dates.append(curdate)
         cas = formatcase(row["Indicator"])
         countries[row["Country"]][cas].append(int(row["Value"]))
-    complete_last_row()
+    dates, countries = complete_last_row(dates, countries)
     del(countries["UK"])
 
 
