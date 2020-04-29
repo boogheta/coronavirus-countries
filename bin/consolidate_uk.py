7f49cfc6ad9a740288df9ce01331a270ee381891
RouxRC <b.ooghe@gmail.com>
2020-04-29 03:11:49 +0200

    handle new England historical values from https://github.com/tomwhite/covid-19-uk-data/commit/e0af5b2d7ad58fd49e6ea464baa8d824de8034db (closes #41)

--- bin/consolidate_uk.py
+++ bin/consolidate_uk.py
@@ -22,16 +22,16 @@ def complete_last_row(dates, countries):
     n = len(countries["UK"]["confirmed"])
     if not n:
         return dates, countries
+    dates = dates[0:n]
     for c in ["Wales", "Northern Ireland", "England", "Scotland"]:
         countries[c]["confirmed"] = countries[c]["confirmed"][0:n]
         countries[c]["deceased"] = countries[c]["deceased"][0:n]
-        dates = dates[0:n]
     for c in ["Wales", "Northern Ireland"]:
         if len(countries[c]["confirmed"]) < n:
             countries[c]["confirmed"].append(0)
             countries[c]["deceased"].append(0)
-    if len(countries["England"]["confirmed"]) < n:
-        for c in ["confirmed", "deceased"]:
+    for c in ["confirmed", "deceased"]:
+        if len(countries["England"][c]) < n:
             countries["England"][c].append(countries["UK"][c][-1] - countries["Wales"][c][-1] - countries["Scotland"][c][-1] - countries["Northern Ireland"][c][-1])
     return dates, countries
 
