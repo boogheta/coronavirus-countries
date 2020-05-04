f5aab50a49224fe69edc3ecf04c409c245b6a5e7
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-05-04 15:00:50 +0200

    silent log

--- bin/update_sources.sh
+++ bin/update_sources.sh
@@ -16,7 +16,7 @@ curl -sfL https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni
 
 # Spain official data
 curl -sfL https://covid19.isciii.es/resources/serie_historica_acumulados.csv > data/serie_historica_acumulados.csv
-if ! head -2 data/serie_historica_acumulados.csv | tail -1 | grep 2020; then
+if ! head -2 data/serie_historica_acumulados.csv | tail -1 | grep 2020 > /dev/null; then
   echo "WARNING: Spain data is missing dates"
   git checkout -- data/serie_historica_acumulados.csv
 fi
